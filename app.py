f051fffe69618ceab6c76035c2f8a3b3e3887e52
Anirudh Pradhan <anipradhan.04@gmail.com>
2024-10-02 00:35:04 +0530

    new update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hey! How ar you doing?"
+    return "Hey! How are you doing?"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.', port=8000)
