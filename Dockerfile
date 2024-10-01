ce4f21682a7e8cb07fc75091cbc9a69b543f1af8
Anirudh Pradhan <anipradhan.04@gmail.com>
2024-10-02 01:11:24 +0530

    updated port

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ copy . .
 
 run pip install -r requirements.txt
 
-expose 8000
+expose 5000
 
 cmd ["python", "app.py"]
--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "Hey! How are you doing?"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0.', port=8000)
+    app.run(debug=True)
